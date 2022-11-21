48bdf0701a8d1e912d5eae32c20cfa2440573060
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-21 14:58:20 +0530

    try

--- src/calculator.java
+++ src/calculator.java
@@ -9,7 +9,7 @@ public class calculator
         int option;
         Scanner sc=new Scanner(System.in);
 
-        while (true)
+
         {
             System.out.println("Enter your choice");
             System.out.println("Press 1 for Addition");
@@ -20,58 +20,68 @@ public class calculator
             option=sc.nextInt();
      }
 
-     switch (option)
-     {
+     switch (option) {
          case 1:
              System.out.println("Enter First Number");
-             a= sc.nextInt();
+             a = sc.nextInt();
              System.out.println("Enter second number");
-             b=sc.nextInt();
-             c=a+b;
-             System.out.println("Sum="+c);
+             b = sc.nextInt();
+             c = a + b;
+             System.out.println("Sum=" + c);
              break;
 
          case 2:
              System.out.println("Enter First Number");
-             a= sc.nextInt();
+             a = sc.nextInt();
              System.out.println("Enter second number");
-             b=sc.nextInt();
-             c=a-b;
-             System.out.println("Substaction="+c);
+             b = sc.nextInt();
+             c = a - b;
+             System.out.println("Substaction=" + c);
              break;
 
          case 3:
              System.out.println("Enter First Number");
-             a= sc.nextInt();
+             a = sc.nextInt();
              System.out.println("Enter second number");
-             b=sc.nextInt();
-             c=a*b;
-             System.out.println("Muliplication="+c);
+             b = sc.nextInt();
+             c = a * b;
+             System.out.println("Muliplication=" + c);
              break;
 
          case 4:
-             System.out.println("Enter First Number");
-             a= sc.nextInt();
-             System.out.println("Enter second number");
-             b=sc.nextInt();
-             c=a/b;
-             System.out.println("Division="+c);
+             try {
+                 System.out.println("Enter First Number");
+                 a = sc.nextInt();
+                 System.out.println("Enter second number");
+                 b = sc.nextInt();
+                 c = a / b;
+                 System.out.println("Division=" + c);
+             }
+             catch (ArithmeticException e)
+             {
+                 System.out.println(e);
+             }
+
+
+
+
              break;
 
          case 5:
              System.exit(0);
+
          default:
              System.out.println("Invaild choice");
 
 
-
+     }}
      }
 
-     }
 
 
 
-    }
 
 
-}
+
+
+
